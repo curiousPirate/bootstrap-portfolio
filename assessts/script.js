5d5a8f4d379ece5a60b3e5a9bea4c19558a03a3d
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-14 13:14:17 +0530

    switch b/n dark and light mode

--- assessts/script.js
+++ assessts/script.js
@@ -1,6 +1,9 @@
-
 let toggleSwitch = document.querySelector('#toggle'); 
 //change data-bs-theme attribute to dark theme 
 toggleSwitch.addEventListener('click', function () { 
-  document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'dark'); 
+  if (toggleSwitch.getAttribute('data-bs-theme') === 'light') {
+    document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'dark');
+  } else {
+    document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'light');
+  }
 });
