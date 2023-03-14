e5ce90817342953e5e7ce0dfc48630229effa28a
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-14 23:54:17 +0530

    now cleared light/dark theme bug and functioning properly

--- assessts/script.js
+++ assessts/script.js
@@ -1,9 +1,22 @@
-let toggleSwitch = document.querySelector('#toggle'); 
-//change data-bs-theme attribute to dark theme 
-toggleSwitch.addEventListener('click', function () { 
-  if (toggleSwitch.getAttribute('data-bs-theme') === 'light') {
-    document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'dark');
+// let toggleSwitch = document.querySelector('#toggle'); 
+// //change data-bs-theme attribute to dark theme 
+// toggleSwitch.addEventListener('click', function () { 
+//   console.log('get');
+//   if (toggleSwitch.getAttribute('data-bs-theme') === 'light') {
+//     document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'dark');
+//   } else {
+//     document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'light');
+//   }
+// });
+
+
+const button = document.querySelector('.btn.btn-light');
+const html = document.querySelector('html');
+
+button.addEventListener('click', () => {
+  if (html.dataset.bsTheme === 'dark') {
+    html.dataset.bsTheme = 'light';
   } else {
-    document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'light');
+    html.dataset.bsTheme = 'dark';
   }
 });
